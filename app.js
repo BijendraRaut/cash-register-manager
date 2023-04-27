2a2f2c11a521eead9b785460c0048d409aaa55a8
Bijendra Raut <bijendraghvm@gmail.com>
2023-04-27 19:47:00 +0530

    input & btn works

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("working");
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkBtn = document.querySelector("#check-remaining");
+
+checkBtn.addEventListener("click", function () {
+  console.log(billAmount.value, cashGiven.value);
+});
